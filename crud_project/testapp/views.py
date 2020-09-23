f606bcddcd6b6d084574b82444e5543d1e498860
arora8055 <aroraaman0101@gmail.com>
2020-09-24 00:34:38 +0530

    Done : PUT operation functionality

--- crud_project/testapp/views.py
+++ crud_project/testapp/views.py
@@ -35,6 +35,7 @@ class EmployeeListCBV(View):
             return HttpResponse(json_data, status=404)
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class EmployeeDetailCBV(View):
     def get(self, request, id, *args, **kwargs):
         try:
@@ -49,3 +50,27 @@ class EmployeeDetailCBV(View):
             for item in pdict:
                 result.append(item['fields'])
             return HttpResponse(json.dumps(result), content_type='application/json')
+
+    def put(self, request, id, *args, **kwargs):
+        try:
+            emp_data = Employee.objects.get(id=id)
+        except Employee.DoesNotExist:
+            return HttpResponse(json.dumps({'msg': 'User Not Found'}), status=404)
+        else:
+            json_data = request.body
+            if not valid_json(json_data):
+                return HttpResponse(json.dumps({'msg': 'Please provide valid JSON data'}), status=404)
+            new_data = json.loads(json_data)
+            old_data = {
+                'eno': emp_data.eno,
+                'ename': emp_data.ename,
+                'esal': emp_data.esal,
+                'eaddr': emp_data.eaddr}
+            old_data.update(new_data)
+            form = EmployeeForm(old_data, instance=emp_data)
+            if form.is_valid():
+                obj = form.save(commit=True)
+                return HttpResponse(json.dumps({'msg': 'Sucessfully registeres'}), status=200)
+            if form.errors:
+                json_data = json.dumps(form.errors)
+                return HttpResponse(json_data, status=404)
