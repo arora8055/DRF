31c6ba0c0e73060cef53cb630872eee0bc7d7c6d
arora8055 <aroraaman0101@gmail.com>
2020-09-23 23:45:45 +0530

    Done GET all employee records

--- crud_project/crud_project/urls.py
+++ crud_project/crud_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from testapp.views import EmployeeDetailCBV
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('emp/', EmployeeDetailCBV.as_view())
 ]
--- crud_project/testapp/views.py
+++ crud_project/testapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+from django.core.serializers import serialize
+from django.http import HttpResponse
+from .models import Employee
+import json
 # Create your views here.
+
+
+class EmployeeDetailCBV(View):
+    def get(self, request):
+        qs = Employee.objects.all()
+        json_data = serialize('json', qs, fields=('eno', 'ename'))
+        pdict = json.loads(json_data)
+        result = []
+        for item in pdict:
+            result.append(item['fields'])
+        return HttpResponse(json.dumps(result), content_type='application/json')
