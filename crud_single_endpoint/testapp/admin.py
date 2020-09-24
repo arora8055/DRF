16bf78680208fe257d0270b8f442ac230e6a6898
arora8055 <aroraaman0101@gmail.com>
2020-09-24 18:15:19 +0530

    Craeted superuser and migrations

--- crud_single_endpoint/testapp/admin.py
+++ crud_single_endpoint/testapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Employee
 # Register your models here.
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['eno', 'esal', 'eaddr', 'esal']
+
+
+admin.site.register(Employee, EmployeeAdmin)
