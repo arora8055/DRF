fd06718ef44faa2cd4622f750a02abcfb7de73e7
arora8055 <aroraaman0101@gmail.com>
2020-09-25 12:44:48 +0530

    Get Functionality implemented

--- crud_single_endpoint/testapp/views.py
+++ crud_single_endpoint/testapp/views.py
@@ -1,35 +1,37 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import Employee
+import json
 from django.http import HttpResponse
 from django.core.serializers import serialize
-import json
-from .models import Employee
-from .utils import valid_json
+from testapp.mixins import SerializeMixin, HttpResponseMixin
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from testapp.utils import is_json
+from testapp.forms import EmployeeForm
 # Create your views here.
 
 
-class EmployeeCRUDCBV(View):
+class EmployeeCRUDCBV(HttpResponseMixin, SerializeMixin, View):
+    def get_object_by_id(self, id):
+        try:
+            emp = Employee.objects.get(id=id)
+        except Employee.DoesNotExist:
+            emp = None
+        return emp
+
     def get(self, request, *args, **kwargs):
         data = request.body
-        if not valid_json(data):
-            return HttpResponse(json.dumps({'msg': 'Please provide valid JSON'}))
+        if not is_json(data):
+            return self.render_to_http_response(json.dumps({'msg': 'plz send valid json data only'}), status=400)
         data = json.loads(data)
-        # id = json.loads(data).get('id', None)
-        # if id is not None:
-        #     try:
-        #         obj = Employee.objects.get(id)
-        #     except Employee.DoesNotExist:
-        #         return HttpResponse(json.dumps({'msg': 'No data found'}))
-        #     else:
-        #         json_data = serialize('json', [obj, ])
-        #         result = []
-        #         for item in json_data:
-        #             result.append(json_data['fields'])
-        #         return HttpResponse(json.dumps(result))
-        # else:
-        #     qs = Employee.objects.all()
-        #     json_data = serialize('json', qs)
-        #     result = []
-        #     for item in json_data:
-        #         result.append(json_data['fields'])
-        #     return HttpResponse(json.dumps(result))
+        id = data.get('id', None)
+        if id is not None:
+            obj = self.get_object_by_id(id)
+            if obj is None:
+                return self.render_to_http_response(json.dumps({'msg': 'No Matched Record Found with Specified Id'}), status=404)
+            json_data = self.serialize([obj, ])
+            return self.render_to_http_response(json_data)
+        qs = Employee.objects.all()
+        json_data = self.serialize(qs)
+        return self.render_to_http_response(json_data)
