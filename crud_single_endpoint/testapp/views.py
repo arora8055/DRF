a3ad80506e97a4d7f858dc4b4fb3618f3bfe481b
arora8055 <aroraaman0101@gmail.com>
2020-09-25 23:21:21 +0530

    PUT request functionality implemented

--- crud_single_endpoint/testapp/views.py
+++ crud_single_endpoint/testapp/views.py
@@ -49,3 +49,33 @@ class EmployeeCRUDCBV(HttpResponseMixin, SerializeMixin, View):
         if form.errors:
             json_data = json.dumps(form.errors)
             return self.render_to_http_response(json_data, status=400)
+
+    def put(self, request, *args, **kwargs):
+        data = request.body
+        if not is_json(data):
+            return self.render_to_http_response(json.dumps({'msg': 'plz send valid json data only'}), status=400)
+        data = json.loads(request.body)
+        id = data.get('id', None)
+        if id is None:
+            return self.render_to_http_response(json.dumps({'msg': 'To perform updation id is mandatory,you should provide'}), status=400)
+        obj = self.get_object_by_id(id)
+        if obj is None:
+            json_data = json.dumps(
+                {'msg': 'No matched record found, Not possible to perform updataion'})
+            return self.render_to_http_response(json_data, status=404)
+        new_data = data
+        old_data = {
+            'eno': obj.eno,
+            'ename': obj.ename,
+            'esal': obj.esal,
+            'eaddr': obj.eaddr
+        }
+        old_data.update(new_data)
+        form = EmployeeForm(old_data, instance=obj)
+        if form.is_valid():
+            form.save(commit=True)
+            json_data = json.dumps({'msg': 'Updated successfully'})
+            return self.render_to_http_response(json_data, status=201)
+        if form.errors:
+            json_data = json.dumps(form.errors)
+            return self.render_to_http_response(json_data, status=400)
