9ec1958f76cdeebb8b40d63cf3121e9cd40c95e2
arora8055 <aroraaman0101@gmail.com>
2020-09-28 17:14:48 +0530

    Models and Migrations part done

--- emp_crud_proj/testapp/models.py
+++ emp_crud_proj/testapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=64)
+    rollno = models.IntegerField()
+    marks = models.IntegerField()
