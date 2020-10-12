073e27fb55f3047f7c95122ab9f39ca89ac198fb
Aman Arora <aroraaman0101@gmail.com>
2020-10-12 19:04:00 +0530

    Get Method API View

--- api_view/testapp/views.py
+++ api_view/testapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from rest_framework import generics
+from testapp.models import Employee
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from testapp.serializers import EmployeeSerializer
 # Create your views here.
-# class EmployeeAPIView(generics.ListAPIView):
-#     queryset =
+
+
+class EmployeeAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        qs = Employee.objects.all()
+        serializer = EmployeeSerializer(qs, many=True)
+        return Response(serializer.data)
